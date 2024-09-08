ac70e36802fdc962cbf6279a5e4e9bda30bda8af
admin <Ishr_Shai@live.ca>
2024-09-08 12:36:38 -0400

    feat: Tried to add the tables accordingly, but there's some cache issues.
    For now all table must be added manually in vercel.

--- app/api/maids/get-maids/route.ts
+++ app/api/maids/get-maids/route.ts
@@ -2,6 +2,6 @@ import { sql } from '@vercel/postgres';
 import { NextResponse } from 'next/server';
  
 export async function GET() {
-  const maids = await sql`SELECT * FROM Maids;`;
+  const maids = await sql`SELECT * FROM merMaids;`;
   return NextResponse.json({ maids }, { status: 200 });
 }
