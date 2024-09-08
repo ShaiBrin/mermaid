8b7b2e2f76f6068cac71424b8bac96ef36579235
admin <Ishr_Shai@live.ca>
2024-09-07 22:12:01 -0400

    feat: Function to create Maid Table

--- app/api/create-maids-table/route.ts
+++ app/api/create-maids-table/route.ts
@@ -1,10 +1,22 @@
 import { sql } from '@vercel/postgres';
 import { NextResponse } from 'next/server';
- 
+
 export async function GET(request: Request) {
   try {
-    const result =
-      await sql`CREATE TABLE Maids ( LastName varchar(255), FirstName varchar(255) );`;
+    const result = await sql`
+      CREATE TABLE Maids (
+        id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+        first_name VARCHAR(255) NOT NULL,
+        last_name VARCHAR(255) NOT NULL,
+        phone_number VARCHAR(20),
+        email VARCHAR(255) UNIQUE NOT NULL,
+        experience_level VARCHAR(255),
+        rating FLOAT,
+        price DECIMAL(10, 2),
+        availability JSONB,
+        location_id UUID REFERENCES Locations(id)
+      );
+    `;
     return NextResponse.json({ result }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
